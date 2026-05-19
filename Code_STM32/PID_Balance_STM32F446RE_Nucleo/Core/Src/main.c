f503548418ba6da4c854f777efdc3efe34853b83
Barnaby Rye <113546986+BarneyRye@users.noreply.github.com>
2026-05-19 12:01:26 +0100

    Finished code

--- Code_STM32/PID_Balance_STM32F446RE_Nucleo/Core/Src/main.c
+++ Code_STM32/PID_Balance_STM32F446RE_Nucleo/Core/Src/main.c
@@ -25,11 +25,28 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef struct {
+  float refAngle;
+  float currentAngle;
+  float Kp;
+  float Ki;
+  float Kd;
+  float lastError;
+  float error;
+  float lastTime;
+  float currentTime;
+  float dt;
+  float errorDerivative;
+  float errorSum;
+  float upperLimit;
+  float lowerLimit;
+  float output;
+} PIDController;
 
 /* USER CODE END PTD */
 
@@ -51,9 +68,12 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+void mapVtoPWM(float voltage, uint16_t *pwmValue);
+void doPIDControl(PIDController *pid);
+float adcToAngle(uint32_t adcValue);
 /* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
+/* USER CODE END PFP */ 
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
@@ -77,7 +97,23 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+  PIDController pid {
+    .refAngle = 0.0f,
+    .currentAngle = 0.0f,
+    .Kp = 50.0f,
+    .Ki = 0.1f,
+    .Kd = 10.0f,
+    .lastError = 0.0f,
+    .error = 0.0f,
+    .lastTime = 0.0f,
+    .currentTime = 0.0f,
+    .dt = 0.0f,
+    .errorDerivative = 0.0f,
+    .errorSum = 0.0f,
+    .upperLimit = 3.0f,
+    .lowerLimit = -3.0f,
+    .output = 0.0f
+  };
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -101,6 +137,17 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+
+    HAL_ADC_START(&hadc1);
+    if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK) {
+      uint32_t adcValue = HAL_ADC_GetValue(&hadc1);
+      pid.currentAngle = adcToAngle(adcValue);
+    }
+
+    doPIDControl(&pid);
+    uint16_t pwmValue;
+    mapVtoPWM(pid.output, &pwmValue);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pwmValue);
 
     /* USER CODE BEGIN 3 */
   }
@@ -162,7 +209,43 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void mapVtoPWM(float voltage, uint16_t *pwmValue) {
+  const float maxVoltage = 6.0f;
+  const float minVoltage = 0.0f;
+  const float tim1period = 999.0f;
 
+  voltage += 3.0f; 
+  
+  if (voltage > maxVoltage) {voltage = maxVoltage;} 
+  else if (voltage < minVoltage) {voltage = minVoltage;}
+
+
+  dutyCycle = (voltage - minVoltage) / (maxVoltage - minVoltage);
+  *pwmValue = (uint16_t)(dutyCycle * 1000);
+}
+
+void doPIDControl(PIDController *pid) {
+  pid->currentTime = HAL_GetTick() / 1000.0f;
+  pid->dt = pid->currentTime - pid->lastTime;
+  pid->lastTime = pid->currentTime;
+
+  pid-> error = pid->refAngle - pid->currentAngle;
+  pid->errorSum += pid->error * pid->dt;
+  pid->errorDerivative = (pid->error - pid->lastError) / pid->dt;
+  pid->lastError = pid->error;
+
+  float pidSum = pid->Kp * pid->error + pid->Ki * pid->errorSum + pid->Kd * pid->errorDerivative;
+  if (pidSum > pid->upperLimit) {pidSum = pid->upperLimit;} 
+  else if (pidSum < pid->lowerLimit) {pidSum = pid->lowerLimit;}
+  pid->output = pidSum;
+}
+
+float adcToAngle(uint32_t adcValue) {
+  const float maxADCValue = 4095.0f; //12-bit ADC
+  const float maxAngle = 270.0f;
+  const float minAngle = 0.0f;
+  return (float(adcValue)/maxADCValue) * (maxAngle - minAngle) + minAngle;
+}
 /* USER CODE END 4 */
 
 /**
